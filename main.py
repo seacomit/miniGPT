52803885b7ae1527a6b26cfcf6c14f4723ccf4e6
Seacomit <86527888+seacomit@users.noreply.github.com>
2024-02-27 14:31:07 -0500

    generate the context and target groupings given the new batch sizes

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 #print(text[:500])
 
 chars = sorted(list(set(text)))
-print(''.join(chars))
-print(len(chars))
+#print(''.join(chars))
+#print(len(chars))
 
 # create maps from character to integer and back
 # the integer is just the index of the character in the list of chars
@@ -20,11 +20,11 @@ itos = {i:ch for i,ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
-print(encode("toast"))
+#print(encode("toast"))
 
 data = torch.tensor(encode(text), dtype=torch.long)
-print(data.shape, data.dtype)
-print(data[:1000])
+#print(data.shape, data.dtype)
+#print(data[:1000])
 
 # split the data into a training set and a validation set
 # used to check for overfitting
@@ -34,16 +34,16 @@ validation_data = data[n:]
 
 # define the chunk size for randomly sampling the training data
 block_size = 8
-print(training_data[:block_size + 1])
+#print(training_data[:block_size + 1])
 
 x = training_data[:block_size]
 y = training_data[1:block_size+1]
 #print(x)
 #print(y)
-for t in range(block_size):
-    context = x[:t+1]
-    target = y[t]
-    print(f"when input is {context}, the target is: {target}")
+#for t in range(block_size):
+#    context = x[:t+1]
+#    target = y[t]
+#    print(f"when input is {context}, the target is: {target}")
 
 torch.manual_seed(41127)
 batch_size = 4 # the number of blocks to process in parallel
@@ -65,3 +65,9 @@ print('targets:')
 print(yb.shape)
 print(yb)
 print('----')
+
+for b in range(batch_size): # batch dimension
+    for t in range(block_size): # time dimension
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        print(f"when input is {context.tolist()} the target is: {target}")
