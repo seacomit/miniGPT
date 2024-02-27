b9d1e5fc292eb1a8037a62fa43503dde9a31a970
Seacomit <86527888+seacomit@users.noreply.github.com>
2024-02-27 12:50:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ text = ""
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-print(len(text))
+#print(len(text))
+#print(text[:500])
+
+chars = sorted(list(set(text)))
+print(''.join(chars))
+print(len(chars))
+
+# create maps from character to integer and back
+# the integer is just the index of the character in the list of chars
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos = {i:ch for i,ch in enumerate(chars)}
+
+# make functions for converting lists of strings to the index based integers and back
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+print(encode("toast"))
